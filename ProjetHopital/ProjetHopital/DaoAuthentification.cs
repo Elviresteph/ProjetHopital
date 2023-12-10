c7f00a99765f229fb1d2237f3d7ab5122e3c48b4
Elviresteph <doungue.elvire@gmail.com>
2023-12-10 13:36:59 +0100

    Authentification ok

---
+++ ProjetHopital/ProjetHopital/DaoAuthentification.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetHopital
+{
+    class DaoAuthentification
+    {
+        //public Authentification connexion(Authentification authentification)
+        //{
+   
+        //    Authentification a = null;
+        //    string connectionString = @"Data Source=DESKTOP-AD02GFS;Initial Catalog=HOPITAL;Integrated Security=True";
+        //    string sql = "select * from Authentification where login= '" + authentification.login +"'" +" "+ "and password = '" + authentification.password +"'";
+
+        //    SqlConnection connection1 = new SqlConnection(connectionString);
+        //    SqlCommand command = new SqlCommand(sql, connection1);
+
+        //    connection1.Open();
+
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    if (reader.Read())
+
+        //        a = new Authentification(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3));
+
+        //    connection1.Close();
+
+        //    return a;
+        //}
+
+        public Authentification connexion(string login, string password)
+        {
+
+            Authentification a = null;
+            string connectionString = @"Data Source=DESKTOP-AD02GFS;Initial Catalog=HOPITAL;Integrated Security=True";
+            string sql = "select * from Authentification where login= '" + login + "'" + " " + "and password = '" + password + "'";
+
+            SqlConnection connection1 = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection1);
+
+            connection1.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+
+                a = new Authentification(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3));
+
+            connection1.Close();
+
+            return a;
+        }
+    }
+}
---
+++ ProjetHopital/ProjetHopital/Medecin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetHopital
+{
+    class Medecin
+    {
+        private string nom;
+        private int num_salle;
+    }
+}
